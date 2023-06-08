8479c7d73e7156e7414b03caee47b37a91c4fee2
Ricky-19 <135237599+Ricky-19@users.noreply.github.com>
2023-06-08 17:51:19 +0200

    Add files via upload

---
+++ CSS/Data_All.js
@@ -0,0 +1,278 @@
+const allProducts = [
+
+    {
+        img: 'Images/MoneteArgento/1.jpeg',
+        name: '5 Franchi 1868 Belgio Re Leopoldo II in argento'
+    },
+
+    {
+        img: 'Images/MoneteArgento/2.jpeg',
+        name: '10 euro 2003 Argento Italia Presidenza Europea FDC'
+    },
+    
+    {
+        img: 'Images/MoneteArgento/3.jpeg',
+        name: '500 LIRE ARGENTO 1985 COLLEGIO DEL MONDO ADRIATICO FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/4.jpeg',
+        name: 'Italia 500 lire 1982 argento Giuseppe Garibaldi FDC'
+    },
+    
+    {
+        img: 'Images/MoneteArgento/5.jpeg',
+        name: 'Italia 500 lire 1981 argento Virgilio Marone FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/6.jpeg',
+        name: 'Italia 500 lire 1985 Presidenza italiana CEE FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/7.jpeg',
+        name: 'Italia 500 lire 1965 argento Caravella FDC'
+    },
+    
+    {
+        img: 'Images/MoneteArgento/8.jpeg',
+        name: 'Italia 500 lire 1966 argento Caravella FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/9.jpeg',
+        name: 'Italia 500 lire 1990 argento Mondiale di Calcio FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/10.jpeg',
+        name: 'Divisionale Francia 1976 con Argento FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/11.jpeg',
+        name: 'Divisionale Francia 1974 con Argento FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/12.jpeg',
+        name: 'Divisionale Francia 1975 con Argento FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/13.jpeg',
+        name: 'Divisionale Italia 1986 Donatello Proof con 2 monete in argento FS'
+    },
+
+    {
+        img: 'Images/MoneteArgento/14.jpeg',
+        name: 'U.S.A. Serie da 8 monete in argento prestigiosa delle Olimpiadi 1983'
+    },
+
+    {
+        img: 'Images/MoneteArgento/15.jpeg',
+        name: '100 Franchi 1989 Monaco Fondo Specchio FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/16.jpeg',
+        name: '10 euro 2005 Spagna Argento Eurostar Fondo specchio'
+    },
+
+    {
+        img: 'Images/MoneteArgento/17.jpeg',
+        name: '5 euro 2021 Italia Argento Maestro Ennio Morricone FDC'
+    },
+
+    {
+        img: 'Images/MoneteArgento/18.jpeg',
+        name: '2022 Italia Divisionale zecca con 5 euro in Argento 100° Monza Autodromo'
+    },
+    
+    {
+        img: 'Images/MoneteRame/1.jpeg',
+        name: '1 ONCIA DI RAME PURO LEGALIZE NATURA FDC'
+    },
+
+    {
+        img: 'Images/MoneteRame/2.jpeg',
+        name: '1 ONCIA DI RAME PURO FREEDOM FDC'
+    },
+
+    {
+        img: 'Images/MoneteRame/3.jpeg',
+        name: '1 ONCIA DI RAME PURO BITCOIN FDC'
+    },
+
+    {
+        img: 'Images/MoneteRame/4.jpeg',
+        name: '1 ONCIA DI RAME PURO PAPA FRANCESCO FDC'
+    },
+
+    {
+        img: 'Images/MoneteStoriche/10.jpeg',
+        name: '5 euro 2020 PROOF Italia Eduardo De filippo FS'
+    },
+
+    {
+        img: 'Images/Banconote/Angola/1.jpeg',
+        name: 'Set 2 banconote Angola 5 e 10 Kwanzas FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Angola/2.jpeg',
+        name: 'Set Angola 2 banconote 200 e 500 Kwanzas 2020 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/1.jpeg',
+        name: 'Congo 1 banconota da 100 Franchi FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/2.jpeg',
+        name: 'Congo banconota da 200 Franchi FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/3.jpeg',
+        name: 'Congo banconota da 10 Franchi 1997 Verdi FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/4.jpeg',
+        name: 'Congo banconota da 20 Franchi 2003 Verdi FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/5.jpeg',
+        name: 'Congo banconota da 50 Franchi 2013 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/6.jpeg',
+        name: 'Congo banconota da 500 Franchi 2013 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Congo/7.jpeg',
+        name: 'Congo banconota da 1000 Franchi 2020 FDS'
+    },
+    
+    {
+        img: 'Images/Banconote/Marocco/1.jpeg',
+        name: 'Marocco Banconota da 20 Dirham FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Marocco/2.jpeg',
+        name: 'Marocco Banconota da 50 Dirham FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Bielorussia/1.jpeg',
+        name: 'Bielorussia 1 banconota da 50 Rubli FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Afghanistan/1.jpeg',
+        name: 'Set 2 banconote Afghanistan 1 e 2 Afgani FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Afghanistan/2.jpeg',
+        name: 'Set 3 banconote Afghanistan 100, 500 e 1000 Afgani FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Cambogia/1.jpeg',
+        name: 'Cambogia 1 banconota da 200 Rials FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Cambogia/2.jpeg',
+        name: 'Cambogia 1 banconota da 50 Rials FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Cambogia/3.jpeg',
+        name: 'Cambogia 1 banconota da 100 Rials FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Bahamas/1.jpeg',
+        name: 'Set 3 banconote da mezzo dollaro Bahamas FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Cina/1.jpeg',
+        name: 'Banconota Cina 100 Yuan'
+    },
+
+    {
+        img: 'Images/Banconote/Cina/2.jpeg',
+        name: 'Set di banconote Cina 1,5,10 yuan 2019 2020 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Cina/3.jpeg',
+        name: '1 banconota Cina 1 yuan FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Indonesia/1.jpeg',
+        name: 'Indonesia 1 banconota da 1000 Rupies FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Indonesia/2.jpeg',
+        name: 'Indonesia banconota da 1000 Rupiah 2022 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Indonesia/3.jpeg',
+        name: 'Indonesia banconota da 2000 Rupiah 2022 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Indonesia/4.jpeg',
+        name: 'Indonesia set 3 banconote 1000, 2000 e 5000 Rupiah 2016 FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Mongolia/1.jpeg',
+        name: 'Set 6 banconote Mongolia FDS vari tagli'
+    },
+
+    {
+        img: 'Images/Banconote/Serbia/1.jpeg',
+        name: 'Serbia Set Banconote 3 pezzi, 10, 20 e 50 Dinara FDS'
+    },
+
+    {
+        img: 'Images/Banconote/Giappone/1.jpeg',
+        name: 'Giappone 1 banconota da 100 yen FDS'
+    }
+];
+
+let allProductsHTML = '';
+let i = 0;
+const isVisibleCheck = JSON.parse(localStorage.getItem('bool'));
+allProducts.forEach((product) => {
+
+    if(isVisibleCheck[i])
+    {
+        allProductsHTML += `
+        <div class = "All-Product">
+            <img src="${product.img}">
+            <div>${product.name}</div>
+        </div>`
+    }
+    i++;
+    document.getElementById("All-Products-Grid").innerHTML = allProductsHTML;
+})
+if (!allProductsHTML)
+{
+    document.getElementById("All-Products-Grid").innerHTML = 'Purtroppo la sua ricerca non ha prodotto risultati'
+}
